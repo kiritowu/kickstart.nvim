9a1e177052b220e648e0b91e22accbcf2cb9d6df
kiritowu <wongzhaowu@gmail.com>
2024-07-15 20:37:55 +0800

    Add ignore command when finding files

--- init.lua
+++ init.lua
@@ -370,6 +370,20 @@ require('lazy').setup({
         pickers = {
           find_files = {
             hidden = true,
+            -- needed to exclude some files & dirs from general search
+            -- when not included or specified in .gitignore
+            find_command = {
+              'rg',
+              '--files',
+              '--hidden',
+              '--glob=!**/.git/*',
+              '--glob=!**/.idea/*',
+              '--glob=!**/.vscode/*',
+              '--glob=!**/build/*',
+              '--glob=!**/dist/*',
+              '--glob=!**/yarn.lock',
+              '--glob=!**/package-lock.json',
+            },
           },
         },
         extensions = {
