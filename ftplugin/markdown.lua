46a33f2a6256f447829d1e28fcc218718be78d9c
kiritowu <wongzhaowu@gmail.com>
2024-07-28 10:45:54 +0800

    [feat] Add ftplugin to set indent space for markdown, svelte and typescriptg

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+vim.opt.tabstop = 4 -- size of a hard tabstop (ts).
+vim.opt.shiftwidth = 4 -- size of an indentation (sw).
+vim.opt.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.opt.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
