d4cbf2927539b915be7f55e6ac3b86e7c4bbdeb9
kiritowu <wongzhaowu@gmail.com>
2024-08-25 13:48:08 +0800

    [feat] Add arrow for quick file navigation

---
+++ lua/custom/plugins/arrow.lua
@@ -0,0 +1,10 @@
+return {
+  -- Harpoon-like file navigator
+  -- https://github.com/otavioschwanck/arrow.nvim
+  'otavioschwanck/arrow.nvim',
+  opts = {
+    show_icons = true,
+    leader_key = ';', -- Recommended to be a single key
+    buffer_leader_key = 'm', -- Per Buffer Mappings
+  },
+}
