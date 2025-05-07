b55a9004d7ebb3026c1a9b5d4af532269b9d155e
kiritowu <wongzhaowu@gmail.com>
2025-05-07 16:47:36 +0800

    [feat] Add better nvim folding support

--- init.lua
+++ init.lua
@@ -164,6 +164,23 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+-- Enable folding
+vim.opt.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method 'textDocument/foldingRange' then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+-- Disable folding on buffer enter
+vim.opt.foldlevelstart = 99
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
